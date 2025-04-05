20eef35b848fea0d25b384b535e01b0b66d9697d
InvisibleTomato <invisibletomato80@gmail.com>
2025-04-05 09:19:18 +0900

    topページ完成

--- js/cursor.js
+++ js/cursor.js
@@ -8,33 +8,27 @@ document.addEventListener("mousemove", function (e) {
 });
 
 // リンクにマウスオーバーしたらカーソルを変更
-const links = document.querySelectorAll("a");
+const interactiveElements = document.querySelectorAll("a");
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("mouseover", function () {
-    cursor.classList.add("cursor--hover");
-    cursor.style.zIndex = "-1"; // カーソルを背面に
-  });
-
-  links[i].addEventListener("mouseout", function () {
-    cursor.classList.remove("cursor--hover");
-    cursor.style.zIndex = "1000"; // カーソルを元のz-indexに戻す
-  });
-}
-
-// hover-whiteクラスにカーソルがホバーした時の処理
-const hoverWhiteElements = document.querySelectorAll(".hover-white");
-
-hoverWhiteElements.forEach(function (element) {
+interactiveElements.forEach(function (element) {
   element.addEventListener("mouseover", function () {
-    this.style.color = "#fdfdfd"; // テキストを白に
-    this.style.transition = "color 0.3s"; // 色の変化を滑らかに
-    cursor.style.zIndex = "-1"; // カーソルを背面に
+    cursor.classList.add("cursor--hover");
   });
 
   element.addEventListener("mouseout", function () {
-    this.style.color = "#0e0e0e"; // テキストを黒に戻す
-    cursor.classList.remove("cursor--hover"); // カーソルを元に戻す
-    cursor.style.zIndex = "1000"; // カーソルを元のz-indexに戻す
+    cursor.classList.remove("cursor--hover");
+  });
+});
+
+//h1にホバーした時にカラーを$whiteに変更
+const h1HoverElements = document.querySelectorAll("h1");
+
+h1HoverElements.forEach(function (element) {
+  element.addEventListener("mouseover", function () {
+    this.classList.add("h1--hover");
+  });
+
+  element.addEventListener("mouseout", function () {
+    this.classList.remove("h1--hover");
   });
 });
