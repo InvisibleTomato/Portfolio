feb717665a6a16273d2cdfee21c153a08aae5026
InvisibleTomato <invisibletomato80@gmail.com>
2025-11-13 16:54:41 +0900

    ✨ クラス名をローディング時専用に変更

--- js/fadein-active.js
+++ js/fadein-active.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const boxes = document.querySelectorAll(".fade-in");
+  const boxes = document.querySelectorAll(".fade-in-loading");
   boxes.forEach(function (box, i) {
     setTimeout(function () {
-      box.classList.add("fade-active");
+      box.classList.add("fade-active-loading");
     }, i * 300); // 各要素を0.3秒ずつ遅らせて表示
   });
 });
